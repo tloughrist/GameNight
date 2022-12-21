0e6fccab6d92e387cc8c0a431713b6fbedca3cd6
timloughrist <tim.loughrist@gmail.com>
2022-12-21 05:33:49 -0600

    finish associations...maybe

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,5 @@
 class Invitation < ApplicationRecord
+    belongs_to :game_night
+    belongs_to :sender, class_name: :User
+    belongs_to :receiver, class_name: :User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_many :attendances
+    has_many :friend_requests
+    has_many :friendships, ->(user) { where("user_id = ? OR friend_id = ?", user.id, user.id) }
+    has_many :friends, through: :friendships
+    has_many :game_nights
+    has_many :invitations
+    has_many :messages
+    has_many :user_games
+    has_many :games, through: :user_games
 end
